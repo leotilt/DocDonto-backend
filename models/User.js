fe3dfaac6ae4d40eb67494892dcf3a0a4c93b76d
Leonardo <leonardooliveiradias1@gmail.com>
2023-01-20 20:47:30 -0300

    add dotenv para mais segurança ao passar a senha

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("projetoXdb", "root", "63524100", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  "projetoXdb",
+  "root",
+  process.env.ROOT_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
 
 const User = sequelize.define("users", {
   id: {
